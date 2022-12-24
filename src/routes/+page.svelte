17e5a48b341777e262926e43ac5d85ba74fa5c56
juunini <juuni.ni.i@gmail.com>
2022-12-24 16:45:33 +0900

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<svelte:head>
+	<title>glTF optimizer</title>
+	<meta name="description" content="Optimize your glTF. Compress to Draco and convert texture to WebP" />
+</svelte:head>
+
+<script lang="ts">
+	import { optimizer } from 'gltf-optimizer'
+	import download from 'get-file-using-a-tag'
+
+	function sizeToMB(size: number) {
+		return (size / 1024 / 1024).toFixed(2)
+	}
+
+	async function onChangeFile() {
+		const size = document.getElementById('size')!
+		size.innerText = 'processing...'
+
+		const file = document.getElementById('file') as HTMLInputElement
+		const glb = await optimizer.web(new Uint8Array(await file.files![0].arrayBuffer()))
+		size.innerText = `${sizeToMB(file.files![0].size)}MB => ${sizeToMB(new Blob([glb]).size)}MB`
+
+		download({
+			fileName: 'compressed.glb',
+			arrayBuffer: glb,
+		})
+	}
+
+</script>
+
+<h1>glTF Optimizer</h1>
+
+<input type="file" id="file" accept=".glb" on:change="{onChangeFile}" />
+
+<br />
+
+<span id="size"></span>
