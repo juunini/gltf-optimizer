93db18297146fb545a833b558500701876029481
juunini <juuni.ni.i@gmail.com>
2022-12-30 22:22:10 +0900

    Install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: 'standard-with-typescript',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  ignorePatterns: [
+    '*.d.ts'
+  ],
+  rules: {
+  }
+}
--- bin/flagOptions.ts
+++ bin/flagOptions.ts
@@ -1,196 +1,196 @@
-import { Options } from "yargs";
+import { Options } from 'yargs'
 
-export const flagOptions: { [key: string]: Options } = {
+export const flagOptions: Record<string, Options> = {
   input: {
-    alias: "i",
-    describe: "Path to the glTF or glb file.",
-    type: "string",
+    alias: 'i',
+    describe: 'Path to the glTF or glb file.',
+    type: 'string',
     normalize: true,
-    demandOption: true,
+    demandOption: true
   },
   output: {
-    alias: "o",
+    alias: 'o',
     describe:
-      "Output path of the glTF or glb file. Separate resources will be saved to the same directory.",
-    type: "string",
-    normalize: true,
+      'Output path of the glTF or glb file. Separate resources will be saved to the same directory.',
+    type: 'string',
+    normalize: true
   },
   binary: {
-    alias: "b",
-    describe: "Convert the input glTF to glb.",
-    type: "boolean",
-    default: false,
+    alias: 'b',
+    describe: 'Convert the input glTF to glb.',
+    type: 'boolean',
+    default: false
   },
   json: {
-    alias: "j",
-    describe: "Convert the input glb to glTF.",
-    type: "boolean",
-    default: false,
+    alias: 'j',
+    describe: 'Convert the input glb to glTF.',
+    type: 'boolean',
+    default: false
   },
   separate: {
-    alias: "s",
+    alias: 's',
     describe:
-      "Write separate buffers, shaders, and textures instead of embedding them in the glTF.",
-    type: "boolean",
-    default: false,
+      'Write separate buffers, shaders, and textures instead of embedding them in the glTF.',
+    type: 'boolean',
+    default: false
   },
   separateTextures: {
-    alias: "t",
-    describe: "Write out separate textures only.",
-    type: "boolean",
-    default: false,
+    alias: 't',
+    describe: 'Write out separate textures only.',
+    type: 'boolean',
+    default: false
   },
   stats: {
-    describe: "Print statistics to console for output glTF file.",
-    type: "boolean",
-    default: false,
+    describe: 'Print statistics to console for output glTF file.',
+    type: 'boolean',
+    default: false
   },
   keepUnusedElements: {
-    describe: "Keep unused materials, nodes and meshes.",
-    type: "boolean",
-    default: false,
+    describe: 'Keep unused materials, nodes and meshes.',
+    type: 'boolean',
+    default: false
   },
   keepLegacyExtensions: {
     describe:
-      "When false, materials with KHR_techniques_webgl, KHR_blend, or KHR_materials_common will be converted to PBR.",
-    type: "boolean",
-    default: false,
+      'When false, materials with KHR_techniques_webgl, KHR_blend, or KHR_materials_common will be converted to PBR.',
+    type: 'boolean',
+    default: false
   },
-  "draco.compressMeshes": {
-    alias: "d",
+  'draco.compressMeshes': {
+    alias: 'd',
     describe:
-      "Compress the meshes using Draco. Adds the KHR_draco_mesh_compression extension.",
-    type: "boolean",
-    default: false,
+      'Compress the meshes using Draco. Adds the KHR_draco_mesh_compression extension.',
+    type: 'boolean',
+    default: false
   },
-  "draco.compressionLevel": {
+  'draco.compressionLevel': {
     describe:
-      "Draco compression level [0-10], most is 10, least is 0. A value of 0 will apply sequential encoding and preserve face order.",
-    type: "number",
-    default: 7,
+      'Draco compression level [0-10], most is 10, least is 0. A value of 0 will apply sequential encoding and preserve face order.',
+    type: 'number',
+    default: 7
   },
-  "draco.quantizePositionBits": {
+  'draco.quantizePositionBits': {
     describe:
-      "Quantization bits for position attribute when using Draco compression.",
-    type: "number",
-    default: 11,
+      'Quantization bits for position attribute when using Draco compression.',
+    type: 'number',
+    default: 11
   },
-  "draco.quantizeNormalBits": {
+  'draco.quantizeNormalBits': {
     describe:
-      "Quantization bits for normal attribute when using Draco compression.",
-    type: "number",
-    default: 8,
+      'Quantization bits for normal attribute when using Draco compression.',
+    type: 'number',
+    default: 8
   },
-  "draco.quantizeTexcoordBits": {
+  'draco.quantizeTexcoordBits': {
     describe:
-      "Quantization bits for texture coordinate attribute when using Draco compression.",
-    type: "number",
-    default: 10,
+      'Quantization bits for texture coordinate attribute when using Draco compression.',
+    type: 'number',
+    default: 10
   },
-  "draco.quantizeColorBits": {
+  'draco.quantizeColorBits': {
     describe:
-      "Quantization bits for color attribute when using Draco compression.",
-    type: "number",
-    default: 8,
+      'Quantization bits for color attribute when using Draco compression.',
+    type: 'number',
+    default: 8
   },
-  "draco.quantizeGenericBits": {
+  'draco.quantizeGenericBits': {
     describe:
-      "Quantization bits for skinning attribute (joint indices and joint weights) ad custom attributes when using Draco compression.",
-    type: "number",
-    default: 8,
+      'Quantization bits for skinning attribute (joint indices and joint weights) ad custom attributes when using Draco compression.',
+    type: 'number',
+    default: 8
   },
-  "draco.uncompressedFallback": {
-    describe: "Adds uncompressed fallback versions of the compressed meshes.",
-    type: "boolean",
-    default: false,
+  'draco.uncompressedFallback': {
+    describe: 'Adds uncompressed fallback versions of the compressed meshes.',
+    type: 'boolean',
+    default: false
   },
-  "draco.unifiedQuantization": {
+  'draco.unifiedQuantization': {
     describe:
-      "Quantize positions of all primitives using the same quantization grid defined by the unified bounding box of all primitives. If this option is not set, quantization is applied on each primitive separately which can result in gaps appearing between different primitives.",
-    type: "boolean",
-    default: false,
+      'Quantize positions of all primitives using the same quantization grid defined by the unified bounding box of all primitives. If this option is not set, quantization is applied on each primitive separately which can result in gaps appearing between different primitives.',
+    type: 'boolean',
+    default: false
   },
-  "texture.webp.enabled": {
-    describe: "Compress the texture using WebP.",
-    type: "boolean",
-    default: true,
+  'texture.webp.enabled': {
+    describe: 'Compress the texture using WebP.',
+    type: 'boolean',
+    default: true
   },
-  "texture.webp.quality": {
-    describe: "Set quality factor between 0 and 100.",
-    type: "number",
-    default: 75,
+  'texture.webp.quality': {
+    describe: 'Set quality factor between 0 and 100.',
+    type: 'number',
+    default: 75
   },
-  "texture.webp.alphaQuality": {
-    describe: "Set transparency-compression quality between 0 and 100.",
-    type: "number",
-    default: 100,
+  'texture.webp.alphaQuality': {
+    describe: 'Set transparency-compression quality between 0 and 100.',
+    type: 'number',
+    default: 100
   },
-  "texture.webp.method": {
-    describe: "Specify the compression method to use, between 0 (fastest) and 6 (slowest). This parameter controls the trade off between encoding speed and the compressed file size and quality.",
-    type: "number",
-    default: 4,
+  'texture.webp.method': {
+    describe: 'Specify the compression method to use, between 0 (fastest) and 6 (slowest). This parameter controls the trade off between encoding speed and the compressed file size and quality.',
+    type: 'number',
+    default: 4
   },
-  "texture.webp.size": {
-    describe: "Set target size in bytes.",
-    type: "number",
+  'texture.webp.size': {
+    describe: 'Set target size in bytes.',
+    type: 'number'
   },
-  "texture.webp.sns": {
-    describe: "Set the amplitude of spatial noise shaping between 0 and 100.",
-    type: "number",
-    default: 80,
+  'texture.webp.sns': {
+    describe: 'Set the amplitude of spatial noise shaping between 0 and 100.',
+    type: 'number',
+    default: 80
   },
-  "texture.webp.filter": {
-    describe: "Set deblocking filter strength between 0 (off) and 100.",
-    type: "number",
+  'texture.webp.filter': {
+    describe: 'Set deblocking filter strength between 0 (off) and 100.',
+    type: 'number'
   },
-  "texture.webp.autoFilter": {
-    describe: "Adjust filter strength automatically.",
-    type: "boolean",
-    default: false,
+  'texture.webp.autoFilter': {
+    describe: 'Adjust filter strength automatically.',
+    type: 'boolean',
+    default: false
   },
-  "texture.webp.sharpness": {
-    describe: "Set filter sharpness between 0 (sharpest) and 7 (least sharp).",
-    type: "number",
-    default: 0,
+  'texture.webp.sharpness': {
+    describe: 'Set filter sharpness between 0 (sharpest) and 7 (least sharp).',
+    type: 'number',
+    default: 0
   },
-  "texture.webp.lossless": {
-    describe: "Encode images losslessly.",
-    type: "boolean",
-    default: false,
+  'texture.webp.lossless': {
+    describe: 'Encode images losslessly.',
+    type: 'boolean',
+    default: false
   },
-  "texture.webp.nearLossless": {
-    describe: "Encode losslessly with an additional lossy pre-processing step, with a quality factor between 0 (maximum pre-processing) and 100 (same as lossless).",
-    type: "number",
-    default: 100,
+  'texture.webp.nearLossless': {
+    describe: 'Encode losslessly with an additional lossy pre-processing step, with a quality factor between 0 (maximum pre-processing) and 100 (same as lossless).',
+    type: 'number',
+    default: 100
   },
-  "texture.webp.crop": {
-    describe: "Crop the image.",
+  'texture.webp.crop': {
+    describe: 'Crop the image.'
   },
-  "texture.webp.crop.x": {
-    type: "number",
+  'texture.webp.crop.x': {
+    type: 'number'
   },
-  "texture.webp.crop.y": {
-    type: "number",
+  'texture.webp.crop.y': {
+    type: 'number'
   },
-  "texture.webp.crop.width": {
-    type: "number",
+  'texture.webp.crop.width': {
+    type: 'number'
   },
-  "texture.webp.crop.height": {
-    type: "number",
+  'texture.webp.crop.height': {
+    type: 'number'
   },
-  "texture.webp.resize": {
-    describe: "Resize the image. Happens after crop.",
+  'texture.webp.resize': {
+    describe: 'Resize the image. Happens after crop.'
   },
-  "texture.webp.resize.width": {
-    type: "number",
+  'texture.webp.resize.width': {
+    type: 'number'
   },
-  "texture.webp.resize.height": {
-    type: "number",
+  'texture.webp.resize.height': {
+    type: 'number'
   },
-  "texture.webp.metadata": {
-    describe: "A list of metadata to copy from the input to the output if present.",
-    type: "array",
-    choices: ["all", "none", "exif", "icc", "xmp"],
-    default: ["none"],
-  },
-};
+  'texture.webp.metadata': {
+    describe: 'A list of metadata to copy from the input to the output if present.',
+    type: 'array',
+    choices: ['all', 'none', 'exif', 'icc', 'xmp'],
+    default: ['none']
+  }
+}
